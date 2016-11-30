3d301d7cf5557ecbf730f0711a18d20efc49415c
Tsuyoshi Kitamoto <tsuyoshi.kitamoto@gmail.com>
2016-11-30 19:32:07 +0900

    TEST

--- m5.js
+++ m5.js
@@ -1,7 +1,16 @@
 // -*- coding: utf-8 -*-
-var geocoder = false;
+var geocoder;
 
 // subroutines
+
+//子要素を全て削除
+function removeAllChilds ( e ) {
+    while ( e.firstChild ) {
+	e.removeChild( e.firstChild );
+    }
+    return e;
+}
+
 function addOptions ( sel, value, text ) {
     var opt = document.createElement( "option" );
     opt.value = value;
@@ -92,6 +101,18 @@ function searchShokuba () {
     });
 }
 
+function move () {
+    var sel = document.getElementById( "shokuba" );
+    if ( sel.selectedIndex < 0 ) {
+	alert( "目的地が選択されていません" );
+	return;
+    }
+    var wp = sel.options[ sel.selectedIndex ].value;
+    document.getElementById( "faddr" ).innerHTML = wp.split( "," )[0] + " " + wp.split( "," )[1];
+    document.getElementById( "stLat" ).innerHTML = wp.split( "," )[3];
+    document.getElementById( "stLng" ).innerHTML = wp.split( "," )[4];
+}
+
 function measure_distance () {
     var rValue = getRadioValue( document.getElementsByName( "yougu" ) );
 
@@ -121,23 +142,26 @@ function measure_distance () {
     };
     var map = new google.maps.Map( document.getElementById( "yougu_map" ), myOptions );
 
+    // 地下鉄を強調表示
     var transitLayer = new google.maps.TransitLayer();
     transitLayer.setMap( map );
 
+    // 出発地点を半径１ｋｍの円で囲む
     var circleOptions = {
-	strokeColor:   "#c71585",
+	strokeColor: "#c71585",
 	strokeOpacity: 0.1,
-	strokeWeight:  3,
-	fillColor:     "#ff1493",
-	fillOpacity:   0.02,
-	clickable:     false,
-	map:           map,
-	center:        new google.maps.LatLng( stLat, stLng ),
-	radius:        1000
+	strokeWeight: 3,
+	fillColor: "#ff1493",
+	fillOpacity: 0.02,
+	clickable: false,
+	map: map,
+	center: new google.maps.LatLng( stLat, stLng ),
+	radius: 1000
     };
     var circle = new google.maps.Circle( circleOptions );
 
-    var col = rValue == "WALKING" ? "'#FF66FF'" : "'#00FFFF'";
+    //
+    var col = rValue == "WALKING" ? '#FF66FF' : '#00FFFF';
     var plo = new google.maps.Polyline( {
 	strokeOpacity: 0.5,
 	strokeWeight: 5,
@@ -149,7 +173,6 @@ function measure_distance () {
 	polylineOptions: plo
     } );
     directionsRenderer.setMap( map );
-    directionsRenderer.setPanel( document.getElementById( "directionsPanel" ) );
 
     // var mode = { WALKING: google.maps.DirectionsTravelMode.WALKING,
     // 		 DRIVING: google.maps.DirectionsTravelMode.DRIVING }[ rValue ];
@@ -166,6 +189,12 @@ function measure_distance () {
 	if ( status == google.maps.DirectionsStatus.OK ) {
 	    directionsRenderer.setDirections( result );
 	}});
+
+    directionsRenderer.setPanel(
+	removeAllChilds(
+	    document.getElementById( "directionsPanel" )
+	));
+    // document.getElementsByClassName( "adp-summary" ).style.borderColor = col;
 }
 
 // ＪＲ線・地下鉄
@@ -202,7 +231,7 @@ function makeCircle( map, latlng ) {
     var circle = new google.maps.Circle( circleOptions );
 }
 
-function getNearStation ( lat, lng ) {
+function getNearStations ( lat, lng ) {
     // objStations ... 別ファイル [ id, name, lat, lng ]
     var result = objStations.map( function ( s ) {
 	var h1 = Math.abs( lat - s[2] );
@@ -228,6 +257,7 @@ function attachMessage( marker, msg ) {
 }
 
 function dispNearStation () {
+    var pinImageUrl = "http://labs.google.com/ridefinder/images/mm_20_orange.png";
     var stLat = document.getElementById( "stLat" ).innerHTML;
     var stLng = document.getElementById( "stLng" ).innerHTML;
     if( stLat == "" ) {
@@ -252,18 +282,18 @@ function dispNearStation () {
     var leftMap = makeMap( objLatLngFrom, "left_station_map" );
     makeMarker( objLatLngFrom, url + "green-dot.png", leftMap );
     makeCircle( leftMap, objLatLngFrom );
-
     var nStations = getNearStations( stLat, stLng ); // [ id, name, lat, lng, dist ]
     for ( var i = 0; i < nStations.length; i++ ) {
-	var objLatLngTo = new google.maps.LatLng( nStations[i][2], nStations[i][3] );
+	var objLatLngTo = new google.maps.LatLng( nStations[i]["lat"],
+						  nStations[i]["lng"] );
 	var dist = google.maps.geometry.spherical.computeDistanceBetween( objLatLngFrom,
 									  objLatLngTo );
 	var marker = new google.maps.Marker( {
 	    position: objLatLngTo,
-	    icon: new google.maps.MarkerImage( "http://labs.google.com/ridefinder/images/mm_20_orange.png" ),
+	    icon: new google.maps.MarkerImage( pinImageUrl ),
 	    map: leftMap
 	} );
-	var str = "(" + ( i + 1 ) + ") " + nStations[i][1] + ": " + Math.floor( dist ) + "m";
+	var str = "(" + ( i + 1 ) + ") " + nStations[i]["name"] + ": " + Math.floor( dist ) + "m";
 	attachMessage( marker, str );
 	google.maps.event.trigger( marker, "click" );
     }
@@ -273,6 +303,21 @@ function dispNearStation () {
     var rightMap = makeMap( latlng, "right_station_map" );
     makeMarker( latlng, url + "red-dot.png", rightMap );
     makeCircle( rightMap, latlng );
+    var nStations = getNearStations( edLat, edLng ); // [ id, name, lat, lng, dist ]
+    for ( var i = 0; i < nStations.length; i++ ) {
+	var objLatLngTo = new google.maps.LatLng( nStations[i]["lat"],
+						  nStations[i]["lng"] );
+	var dist = google.maps.geometry.spherical.computeDistanceBetween( objLatLngFrom,
+									  objLatLngTo );
+	var marker = new google.maps.Marker( {
+	    position: objLatLngTo,
+	    icon: new google.maps.MarkerImage( pinImageUrl ),
+	    map: rightMap
+	} );
+	var str = "(" + ( i + 1 ) + ") " + nStations[i]["name"] + ": " + Math.floor( dist ) + "m";
+	attachMessage( marker, str );
+	google.maps.event.trigger( marker, "click" );
+    }
 }
 
 function setupShokuba () {
